4a03b0dbc0008f9f9baea9cef1b45b93b5b1f8e4
pkumar1618 <prashanthch943@gmail.com>
2026-06-12 11:20:00 +0530

    variables using with read command

--- 05-variable-with-read-command.sh
+++ 05-variable-with-read-command.sh
@@ -2,12 +2,12 @@
 
 echo "Please enter your username:"
 
-read USERNAME # read command is hiding the usenames and password for security reson
+read -s USERNAME # read command is hiding the usenames and password for security reson
 
 echo "USERNAME enterd is: $USERNAME"
 
 echo "Please enter you password:"
 
-read PASSWORD
+read -s PASSWORD
 
 echo "PASSWORD enterd is: $PASSWORD"
