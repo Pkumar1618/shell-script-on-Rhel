0253e515417a63fc303b866dd108159948649919
pkumar1618 <prashanthch943@gmail.com>
2026-06-12 16:16:53 +0530

    git installation using script

---
+++ 11-installation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=(id -u)
+# echo "User id is: $USERID"
+
+if [ $USERID -ne 0 ]
+then
+   echo "Please run this script with root priveleges"
+   exit 1
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+   echo "Git is not installed, going to install.."
+   dnf install git -y
+   if [ $? -ne 0 ]
+   then
+      echo "Git installation is not success, check it.."
+      exit 1
+    else
+        echo "Git installation is success"  
+else
+   echo "Git is already installed, nothing to do.."
+fi
