2cfcb7447e8fdd3407302ce7213ef38af2f39695
pkumar1618 <prashanthch943@gmail.com>
2026-06-12 10:29:11 +0530

    conversation between two persons

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "prashanth:: hello venkatesh how are you?"
+echo "venkatesh:: I am fine prashanth. how are you doing?"
+echo "prashanth:: I am doing good. what's going on?"
+echo "venaktesh:: I am learning new things."
