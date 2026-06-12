04827cd29c5df80f8b827bd69e2e3028ee7679c9
pkumar1618 <prashanthch943@gmail.com>
2026-06-12 11:02:55 +0530

    varibles are passed as a args

---
+++ 04-variable with arg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "${PERSON1}:: Hi ${PERSON2} are you. what are you doing?"
+echo "${PERSON2}:: I am fine ${PERSON1}. how are you doing? ${PERSON2}"
+echo "${PERSON1}:: I am Good ${PERSON2}. what's is going on? ${PERSON1}"
