514b03f99345cc24eb47ac5719eb14ab5a23e78f
pkumar1618 <prashanthch943@gmail.com>
2026-06-12 16:33:36 +0530

    mysql installtion using shell script

---
+++ 12-mysql-installation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script with root priveleges"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "Mysql is not installed, going to install"
+    dnf install mysql
+    if [ $? -ne 0 ]
+    then
+        echo "Mysql installation is failure. check it.."
+        exit 1
+    else
+        echo "mysql installation is success."
+    fi
+else
+    echo "Mysql is already installed nothing to do.."
+fi
