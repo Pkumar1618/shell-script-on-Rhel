7eda6031b35acfa4a0792496dfe2a2ac404ebad9
pkumar1618 <prashanthch943@gmail.com>
2026-06-12 17:47:17 +0530

    listing the git using validation

---
+++ 14-functions-validation.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is...FAILED"
+        exit 1
+    else
+        echo "$2 is...SUCCESS"
+    fi
+
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root priveleges."
+    exit 1
+fi
+
+dnf list installed git
+
+VALIDATE $? "Listing Git"
