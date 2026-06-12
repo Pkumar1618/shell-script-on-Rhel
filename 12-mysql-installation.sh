f69c7bdc46ac456d3d4963e15ef1b243a794bec4
pkumar1618 <prashanthch943@gmail.com>
2026-06-12 16:56:47 +0530

    mysql installtion using shell script

--- 12-mysql-installation.sh
+++ 12-mysql-installation.sh
@@ -4,8 +4,8 @@ USERID=(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "please run this script with root priveleges"
-    exit 1
+   echo "Please run this script with root priveleges"
+   exit 1
 fi
 
 dnf list installed mysql
