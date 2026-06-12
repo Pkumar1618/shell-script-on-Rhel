ebae41a5cbc66e1686fe3bfad1d7b2324a30a45a
prashanth <prashanthch943@gmail.com>
2026-06-13 00:05:11 +0530

    using for loop

--- 18-loop.sh
+++ 18-loop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# genral for for loop syntax in programming language
+# forloop syntax in programming language
 
 for (i=0; i>=100; i++){
     print i;
 }
 
-in shell script for loop syntax is
+# in shell script for loop syntax is
 
 for i in 1 2 3 4 5 6 7 8 9 10
 do 
