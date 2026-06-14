814ba0f685deee96bfbb587cd255dad5877baaa8
prashanth <prashanthch943@gmail.com>
2026-06-14 22:46:01 +0530

    deleting old file using crontab

--- 22-delete-old-file-using-cron.sh
+++ 22-delete-old-file-using-cron.sh
@@ -14,7 +14,7 @@ else
     exit 1
 fi
 
-FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +12)
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
 echo "Files: $FILES"
 
 # dont use line, it is reserverd word
